74dd286d086262402ba96826874458c6d4288e01
Jon Apostol <jrcapostol@gmail.com>
2018-10-03 07:46:21 -0700

    initial commit. added player movement to character

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float _movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveCharacter();
+	}
+
+    private void MoveCharacter(){
+        var horizontalMovement = Input.GetAxis("Horizontal") 
+                                      * Time.deltaTime * _movementSpeed;
+        var verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime
+                                    * _movementSpeed;
+
+        transform.position += new Vector3(horizontalMovement, 0, verticalMovement);
+    }
+}
