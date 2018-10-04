2c3a9bd19e1059ff5d05130b87efe6fd58c2197e
Jon Apostol <jrcapostol@gmail.com>
2018-10-03 18:23:23 -0700

    camera now moves towards the player, player can now call all followers with space key

---
+++ Assets/Scripts/FollowerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerScript : MonoBehaviour {
+
+    public float _followerMoveSpeed = 5;
+
+    private Vector3 _playerLocation;
+    private bool _calledByPlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(_calledByPlayer){
+            MoveTowardsPlayer();
+        }
+	}
+
+    private void MoveTowardsPlayer(){
+        transform.position =
+            Vector3.MoveTowards(transform.position,
+                                _playerLocation, _followerMoveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, _playerLocation) < 0.5f){
+            _calledByPlayer = false;
+        }
+    }
+
+
+    public void GoToPlayerLocation(Vector3 playerLocation){
+        _calledByPlayer = true;
+        _playerLocation = playerLocation;
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     public float _movementSpeed;
+    public GameObject _cameraObject;
+    public float _cameraMovementSpeed = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +16,20 @@ public class PlayerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         MoveCharacter();
+        CallFollowers();
+        MoveCameraTowardsPlayer();
 	}
+
+    private void MoveCameraTowardsPlayer(){
+        var mainCamera = _cameraObject.GetComponent<Camera>();
+
+        if(mainCamera != null){
+            var cameraPosition = _cameraObject.transform.position;
+            mainCamera.transform.position =
+                Vector3.Lerp(cameraPosition, transform.position + new Vector3(0, 2.5f, -5f), _cameraMovementSpeed * Time.deltaTime);
+        }
+
+    }
 
     private void MoveCharacter(){
         var horizontalMovement = Input.GetAxis("Horizontal") 
@@ -23,5 +38,13 @@ public class PlayerScript : MonoBehaviour {
                                     * _movementSpeed;
 
         transform.position += new Vector3(horizontalMovement, 0, verticalMovement);
+    }
+
+    private void CallFollowers(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("calling followers...");
+            var worldObject = transform.parent;
+            worldObject.BroadcastMessage("GoToPlayerLocation", transform.position, SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
